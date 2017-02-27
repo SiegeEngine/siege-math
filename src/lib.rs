35df1997ead4382a03cf37c416369ed99b5c8231
Mike Dilger <mike@optcomp.nz>
2017-02-28 11:47:28 +1300

    Collapse submodule math back to main lib

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,9 @@ extern crate float_cmp;
 // FIXME:  once simd is part of std and stable, use it
 // extern crate simd;
 
-pub mod math;
+pub mod vector;
+pub use self::vector::{Vec2, Vec3, Vec4};
+
+pub mod matrix;
+pub use self::matrix::{Mat2, Mat3, Mat4};
+
--- src/math/mod.rs
+++
@@ -1,7 +0,0 @@
-
-pub mod vector;
-pub use self::vector::{Vec2, Vec3, Vec4};
-
-pub mod matrix;
-pub use self::matrix::{Mat2, Mat3, Mat4};
-
